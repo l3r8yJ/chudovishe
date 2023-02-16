8ee8cb95264f008f4f0d1cfc6c24fd89698506fc
Ivan <clicker.heroes.acg@gmail.com>
2023-02-16 05:44:10 +0300

    list

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,43 @@
-// A struct with two fields
-#[derive(Debug)]
-struct Point {
-    x: f32,
-    y: f32,
+use crate::List::*;
+
+enum List {
+    Cons(u32, Box<List>),
+    Nil,
 }
 
-// Structs can be reused as fields of another struct
-#[derive(Debug)]
-struct Rectangle {
-    // A rectangle can be specified by where the top left and bottom right
-    // corners are in space.
-    top_left: Point,
-    bottom_right: Point,
-}
+impl List {
+    fn new() -> List {
+        Nil
+    }
 
-fn rectangle_area(rect: &Rectangle) -> f32 {
-    ((rect.bottom_right.x - rect.top_left.x) * (rect.bottom_right.y - rect.top_left.y))
-        .abs()
-}
+    fn add(self, e: u32) -> List {
+        Cons(e, Box::new(self))
+    }
 
-fn square(x: f32, y: f32, bottom_right: Point) -> (f32, f32) {
-    ((bottom_right.x - x).abs(), (bottom_right.y - y).abs())
+    fn len(&self) -> u32 {
+        match *self {
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0
+        }
+    }
+
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                format!("{}. {}", head, tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            }
+        }
+    }
 }
 
 fn main() {
-    let r = Rectangle {
-        top_left: Point { x: 1.0, y: 10.0 },
-        bottom_right: Point { x: 5.0, y: 1.0 },
-    };
-    println!("{:?} area is {}", r, rectangle_area(&r));
-    let s = square(-3.0, 6.0, Point { x: 4.0, y: -2.0 });
-    println!("h is {:.2}, w is {:.2}", s.0, s.1)
+    let mut lst = List::new();
+    println!("List before additions: {}, length equals {}", lst.stringify(), lst.len());
+    lst = lst.add(4);
+    lst = lst.add(2);
+    lst = lst.add(5);
+    println!("List after additions: {}, length equals {}", lst.stringify(), lst.len());
 }
